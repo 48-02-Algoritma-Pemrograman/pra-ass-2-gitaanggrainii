14b8054d024bd952542d34a472b65f580c1affde
gitaanggrainii <gitaanggraini2005@gmail.com>
2024-12-07 23:22:40 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,15 +1,25 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Gaji{
     public static int masaKerja;
-        int gajiBulanan = gaji(masaKerja);
-        int lemburBulanan = lembur(masaKerja);
 
-        lemburBulanan*=jamLembur;
-        gajiBulanan+=lemburBulanan;
+    public static int gaji(int masaKerja){
+    if (masaKerja<5) {
+        return 5000000;
+    } else if (masaKerja>=5&&masaKerja<=10) {
+        return 7500000;
+    } else 
+    return 10000000;
+    }
 
-        System.out.println(gajiBulanan);
-       
+    public static int lembur(int masaKerja){
+        if (masaKerja<5) {
+            return 40000;
+        } else if (masaKerja>=5&&masaKerja<=10) {
+            return 50000;
+        } else 
+        return 75000;
+    }
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
@@ -17,7 +27,7 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
-     
+
         int gajiBulanan = gaji(masaKerja);
         int lemburBulanan = lembur(masaKerja);
 
@@ -31,6 +41,7 @@ public class Gaji{
         System.out.println(Gaji.class.getDeclaredMethods().length);
         System.out.println(Gaji.class.getDeclaredFields().length);
         
+        input.close();
     }
     
 }
